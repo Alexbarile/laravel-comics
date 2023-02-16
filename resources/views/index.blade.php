849b06dcddd130f61a6e05d01066c91fe6c745ef
Alexbarile <alexbarile93fg@yahoo.it>
2023-02-16 21:25:37 +0100

    stilizzato main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+    
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,19 +9,53 @@
 </head>
 
 <body>
+
+    {{-- HEADER --}}
+
     @include('partials.header')
+
+     {{-- MAIN --}}
+
     <main>
-        <!-- jumbotron -->
+
+        {{-- jumbotron --}}
 
         <div class="jumbo">
         </div>
-        
-        @foreach($products as $product)
-        <img src="{{$product['thumb']}}" alt="">
-        <h1>{{$product['title']}}</h1>
-        @endforeach
+
+        {{-- card --}}
+
+        <div class="black-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-info">
+                        <h4>Current series</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        @foreach($products as $product)
+                        <div class="album-card">
+                            <div class="card-img">
+                                <img src="{{$product['thumb']}}" alt="">
+                            </div>
+                            <h3>{{$product['title']}}</h3>  
+                        </div>
+                        
+                        @endforeach
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <button><a href="#">Load More</a></button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
+
+     {{-- FOOTER --}}
+
     @include('partials.footer')
 </body>
-
 </html>
