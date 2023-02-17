b7267da288ce5fb43aac4e2665ecb929467ce866
Alexbarile <alexbarile93fg@yahoo.it>
2023-02-17 22:40:47 +0100

    inizio stilizzazione detail_comics.php

--- resources/views/detail_comics.blade.php
+++ resources/views/detail_comics.blade.php
@@ -2,7 +2,41 @@
 @section('title', 'Details | Comics')
 
 @section('content')
-<h2>{{($single['series'])}}</h2>
+<div class="container">
+    <div class="row d-flex justify-content-between align-items-center">
+
+        {{-- SX PART --}}
+
+        <div class="col">
+            <div>
+                <h2>{{($single['title'])}}</h2>
+            </div>
+            <div class="my-container">
+                <div class="row">
+                    <div class="col">
+                        <p>U.S. Price: <span>{{($single['price'])}}</span></p>
+                    </div>
+                    <div class="col">
+                        <p>AVAILABLE</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+         {{-- DX PART --}}
+
+        <div class="col d-flex justify-content-center">
+            <div class="content d-flex flex-column">
+                <div class="d-flex justify-content-end">
+                    <h5>advertisement</h5>
+                </div>
+                <div>
+                    <img src="{{ Vite::asset('resources/img/adv.jpg')}}" alt="adv">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 
 @endsection
