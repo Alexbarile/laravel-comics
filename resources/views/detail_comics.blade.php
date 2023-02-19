99772ecc48704aa843b18dca07a1108c2c17f226
Alexbarile <alexbarile93fg@yahoo.it>
2023-02-19 18:40:04 +0100

    aggiunti i dropdown

--- resources/views/detail_comics.blade.php
+++ resources/views/detail_comics.blade.php
@@ -37,7 +37,7 @@
                     <div class="col-70">
                         <div class="row-mini d-flex">
                             <div class="col d-flex">
-                                <p>U.S. Price:</p><span>{{($single['price'])}}</span>
+                                <p>U.S. Price:</p><span class="text-white fw-semibold">{{($single['price'])}}</span>
                             </div>
                             <div class="col d-flex justify-content-end">
                                 <p>AVAILABLE</p>
@@ -45,7 +45,18 @@
                         </div>                 
                     </div>
                     <div class="col-30 d-flex justify-content-center">
-                        <span>Check Availability</span>
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle  fw-semibold" href="#null" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Check Availability
+                              </a>
+                              <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item border-0 p-2" href="#null">Prova 1</a></li>
+                                <li><a class="dropdown-item border-0 p-2" href="#null">Prova 2</a></li>
+                                <li><a class="dropdown-item border-0 p-2" href="#null">Prova 3</a></li>
+                              </ul>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -83,7 +94,7 @@
                         <p>Art by:</p> 
                     </div>
                     <div class="col-70">
-                        <a href="#"> {{implode(', ', $single['artists'])}} </a>
+                        <a href="#null"> {{implode(', ', $single['artists'])}} </a>
                     </div>
                 </div>
                 <div class="second-line d-flex justify-content-between align-self-baseline">
@@ -91,7 +102,7 @@
                         <p>Written by:</p> 
                     </div>
                     <div class="col-70">
-                        <a href="#"> {{implode(', ', $single['writers'])}} </a>
+                        <a href="#null"> {{implode(', ', $single['writers'])}} </a>
                     </div>
                 </div>
             </div>
@@ -104,7 +115,7 @@
                         <p>Series:</p> 
                     </div>
                     <div class="col-70">
-                        <a href="#"> {{$single['series']}} </a>
+                        <a href="#null"> {{$single['series']}} </a>
                     </div>
                 </div>
                 <div class="second-line d-flex justify-content-between align-self-baseline">
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,9 +3,22 @@
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <ul>
-                        <li>DC POWER&#153; VISA&#174;</li>
-                        <li>ADDITIONAL DC SITES</li>
+                    <ul class="menu">
+                        <li><a class="nav-link" href="#null">DC POWER&#153; VISA&#174;</a></li>
+                        <li>
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown">
+                                  <a class="nav-link dropdown-toggle" href="#null" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    ADDITIONAL DC SITES 
+                                  </a>
+                                  <ul class="dropdown-menu dropdown-menu-dark">
+                                    <li><a class="dropdown-item" href="#null">Prova 1</a></li>
+                                    <li><a class="dropdown-item" href="#null">Prova 2</a></li>
+                                    <li><a class="dropdown-item" href="#null">Prova 3</a></li>
+                                  </ul>
+                                </li>
+                            </ul>
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -30,31 +43,42 @@
                         <a href="{{ route('characters')}}">Characters</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'comics' ? 'active' : ''}}">
-                        <a href="">Comics</a>
+                        <a href="#null">Comics</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'movies' ? 'active' : ''}}">
-                        <a href="">Movies</a>
+                        <a href="#null">Movies</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'tv' ? 'active' : ''}}">
-                        <a href="">Tv</a>
+                        <a href="#null">Tv</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'games' ? 'active' : ''}}">
-                        <a href="">Games</a>
+                        <a href="#null">Games</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'collectibles' ? 'active' : ''}}">
-                        <a href="">Collectibles</a>
+                        <a href="#null">Collectibles</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'videos' ? 'active' : ''}}">
-                        <a href="">Videos</a>
+                        <a href="#null">Videos</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'fans' ? 'active' : ''}}">
-                        <a href="">Fans</a>
+                        <a href="#null">Fans</a>
                     </li>
                     <li class="p-3 {{ Route::currentRouteName() == 'news' ? 'active' : ''}}">
-                        <a href="">News</a>
+                        <a href="#null">News</a>
                     </li>
-                    <li class="p-3 {{ Route::currentRouteName() == 'shop' ? 'active' : ''}}">
-                        <a href="">Shop</a>
+                    <li class="p-3{{ Route::currentRouteName() == 'shop' ? 'active' : ''}}">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle border-0 p-0" href="#null" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Shop
+                              </a>
+                              <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><a class="dropdown-item border-0 p-2" href="#null">Prova 1</a></li>
+                                <li><a class="dropdown-item border-0 p-2" href="#null">Prova 2</a></li>
+                                <li><a class="dropdown-item border-0 p-2" href="#null">Prova 3</a></li>
+                              </ul>
+                            </li>
+                        </ul>
                     </li>
                     <li class="p-3">
                         <input type='text' placeholder="Search">
